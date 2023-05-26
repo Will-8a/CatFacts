9e40c9699723ac743748cb13730732fd2e17d855
Will-8a <qwertywo361@gmail.com>
2023-05-26 11:54:54 -0400

    Add CatFacts section to Svelte file
    This commit adds cat information and a default fun fact

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,14 @@
 
 <h1>CatFacts</h1>
 
+<main>
+    <p>Cats are a domesticated species of small, carnivorous mammal. They are commonly kept as house pets, but they can also be farm cats or feral cats. Here, you can find some random, interesting facts about cats:</p>
+</main>
+
+<div>
+    <p id='fact_paragraph'>Julius Ceasar, Henri II, Charles XI, and Napoleon were all afraid of cats.</p>
+</div>
+
 <style>
 
 </style>
